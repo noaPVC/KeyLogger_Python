e24d6b1608dbc9d1172e7f135bd6c56eea9323ea
Perkovic Noa <76920918+noaPVC@users.noreply.github.com>
2021-02-05 14:44:33 +0100

    Update txtHandling.py

--- txtHandling.py
+++ txtHandling.py
@@ -1,18 +1,18 @@
 
-
 def getChars(path):
     file = open(path, "r")
     data = file.read()
     number_of_characters = len(data)
     return number_of_characters
 
-
+# file content deletion
 def deleteContentFile(path):
     file = open(path, "r+")
     file. truncate(0)
     file. close()
 
 
+# method for deleting the last char instead of just typing [BACKSLASH]
 def deleteLast(path):
     file = open(path, 'r')
     lines = file.readlines()
